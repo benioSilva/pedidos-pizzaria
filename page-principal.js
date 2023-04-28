8d96c0bcf9ca7b73ce708f0e00702485ce2aeff2
benioSilva <beniosilva10000@gmail.com>
2023-04-28 18:51:37 -0300

    pedidos pizzaria

---
+++ page-principal.js
@@ -0,0 +1,12 @@
+//var pizzasObjetos = JSON.parse()
+ var listaPizza = document.getElementsByClassName("pizzas")
+ console.log(listaPizza)
+ var pizzaNomes = {1: listaPizza[0].innerHTML, 2: listaPizza[1].innerHTML, 3: listaPizza[2].innerHTML, 4: listaPizza[3].innerHTML}
+ console.log(pizzaNomes)
+ //let objetoNome = pizzaNomes[formula para o comprimento do objeto]
+ function nomeDaPizza(){
+    
+ }   
+    
+ 
+ 
---
+++ pedidos.js
@@ -0,0 +1,8 @@
+var sabor =  document.getElementById("saborNumero1")
+console.log(sabor)
+var btnEnviar = document.getElementById("btnEnviar")
+
+btnEnviar.addEventListener("click", function(event){
+    event.preventDefault();
+    
+})
