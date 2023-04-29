bcca58c7f91f810994030edb14fa609152424a8a
benioSilva <beniosilva10000@gmail.com>
2023-04-29 19:02:00 -0300

    finalizando a base (falta colocar alguns styles)

--- lista-pedidos.js
+++ lista-pedidos.js
@@ -1,32 +1,32 @@
-var jubilauDoStorage = localStorage.getItem("jubilau");
-var jubilauObjeto = JSON.parse(jubilauDoStorage);
-preencherTabela() /* preencherTabela recebe uma função chamada verificar com um parametro*/
+var infoDoStorage = localStorage.getItem("info");
+var infoObjeto = JSON.parse(infoDoStorage);
+preencherTabela() 
     function verificar(num){//verificar me diz que jubilauObjeto é igual a um filter que vai utilizar uma condição
-      jubilauObjeto = jubilauObjeto.filter(function(element,index){
+      infoObjeto = infoObjeto.filter(function(element,index){
         if (index!=num){//se o valor index do jubilauObjeto for diferente do valor ele me retorna true 
           return true
         } else{
           return false
         }
       })/* função filter para filtrar os index usando o if para comparação de dados */
-      localStorage.setItem("jubilau", JSON.stringify(jubilauObjeto))/* depois do filtro ele insere os dados do jubilau no localStorage*/ 
+      localStorage.setItem("info", JSON.stringify(infoObjeto))/* depois do filtro ele insere os dados do jubilau no localStorage*/ 
       preencherTabela()
-      console.log(jubilauObjeto)
+      console.log(infoObjeto)
 
     }
 
     function preencherTabela(){ //declarando a função preencherTabela
-      document.getElementById("corpo").innerHTML=""
-      jubilauObjeto.forEach(function (element, index) {//forEach me tras dois valores (element e index)  
+      document.getElementById("corpo1").innerHTML=""
+      infoObjeto.forEach(function (element, index) {//forEach me tras dois valores (element e index)  
       console.log(element)
-      document.getElementById("corpo").innerHTML+='<tr>'+ 
+      document.getElementById("corpo1").innerHTML+='<tr>'+ 
                         '<th scope="row">'+ (index+1) +'</th>'+
                         '<td>'+element.nome +'</td>'+
                         '<td>'+ element.endereco +'</td>'+
                         '<td>'+ element.telefone +'</td>'+
                         '<td>'+ element.sabor +'</td>'+
                         '<td>'+ element.valor +'</td>'+
-                        '<td><button onclick="verificar('+index+')" type="button" class="btn btn-secondary">click</button></td>'
+                        '<td><button onclick="verificar('+index+')" type="button">click</button></td>'
                         /* na função onclick eu inseri um botao que ao rodar a função verificar, ele me dá o valor do index(indice)
                         como ele vai acabar sendo igual ao valor de num pq o meu parametro passa a ser o mesmo index, ele passa a 
                         retornar o valor de false na validação do "if" o que o apaga da minha lista de cadastro (apenas os que eu clicar)
--- pedidos.js
+++ pedidos.js
@@ -1,2 +1,49 @@
-var pizzasDoStorage = localStorage.getItem("catalogosDePizzas")
-var pizzasObjetos = JSON.parse(pizzasDoStorage)
+    // var pizzasDoStorage = localStorage.getItem("catalogosDePizzas")
+    // var pizzasObjetos = JSON.parse(pizzasDoStorage)
+    // var precoDoStorage = localStorage.getItem("precoPizzas")
+    // var precoObjetos = JSON.parse(precoDoStorage)
+
+    var nome = document.getElementById("nome");
+    var endereco = document.getElementById("endereco");
+    var telefone = document.getElementById("telefone"); 
+    var sabor = document.getElementById("saborNumero"); 
+    var valor = document.getElementById("valor"); 
+    var btnEnviar = document.getElementById("btnEnviar");
+    var infoDoStorage = localStorage.getItem("info");
+    var infoObjeto = JSON.parse(infoDoStorage);
+    
+    //console.log(jubilauDoStorage)
+   // console.log(jubilauObjeto)
+    
+   
+    
+    /*if (jubilauDoStorage != null) {
+      alert(jubilauObjeto.nome + '\n' + jubilauObjeto.email + '\n' + jubilauObjeto.idade);
+    }*/
+
+    btnEnviar.addEventListener("click", function(event){
+      event.preventDefault();
+      
+     
+   
+  
+      var infoDoStorage = localStorage.getItem("info");
+      var infoObjeto = JSON.parse(infoDoStorage);  
+      
+      let dados = {nome: nome.value, endereco: endereco.value, telefone: telefone.value, sabor: sabor.value, valor: valor.value};
+      console.log(infoDoStorage)
+      if (infoDoStorage == null){    
+        const lista = [dados] 
+        localStorage.setItem("info", JSON.stringify(lista))
+      } else {
+        infoObjeto.push(dados)
+        localStorage.setItem("info",JSON.stringify(infoObjeto))
+      }
+      nome.value = "" 
+      endereco.value = ""
+      telefone.value = ""
+      sabor.value = ""
+      valor.value = ""
+    })
+
+     
