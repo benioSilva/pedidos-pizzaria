109f17d377bc199a700ef39fd87bbec2ae74a108
benioSilva <beniosilva10000@gmail.com>
2023-05-03 17:36:24 -0300

    projeto finalizado s/ css

--- lista-pedidos.js
+++ lista-pedidos.js
@@ -1,6 +1,7 @@
 var infoDoStorage = localStorage.getItem("info");
 var infoObjeto = JSON.parse(infoDoStorage);
 preencherTabela() 
+
     function verificar(num){//verificar me diz que jubilauObjeto é igual a um filter que vai utilizar uma condição
       infoObjeto = infoObjeto.filter(function(element,index){
         if (index!=num){//se o valor index do jubilauObjeto for diferente do valor ele me retorna true 
@@ -14,6 +15,36 @@ preencherTabela()
       console.log(infoObjeto)
 
     }
+    // var confirmar = document.getElementById("confirmar")
+                      
+    //                     confirmar.addEventListener("click", function(event){
+    //                      event.preventDefault();
+                         
+ 
+    //                      document.getElementById("troca").innerHTML = "Pedido Entregue"
+                         
+    //                   });
+    
+    // let confirmacaoEntrega = document.getElementById("confirmar");
+    // confirmacaoEntrega.addEventListener('click', StatusEntregue())
+    
+    // let confirmar = document.getElementById("confirmar");
+    //                   confirmar.addEventListener('click', trocarStatus())
+                       
+                        
+    //                     function trocarStatus (){
+    //                       document.getElementById("trocar").innerHTML="Pedido Entregue"
+    //                     }
+    
+        
+      function confirmarEntrega(index){
+        console.log(index)
+        infoObjeto[index].status="Pedido Entregue"
+        preencherTabela()
+        localStorage.setItem("info", JSON.stringify(infoObjeto))
+      }
+                       
+  
 
     function preencherTabela(){ //declarando a função preencherTabela
       document.getElementById("corpo1").innerHTML=""
@@ -26,21 +57,32 @@ preencherTabela()
                         '<td>'+ element.telefone +'</td>'+
                         '<td>'+ element.sabor +'</td>'+
                         '<td>'+ element.valor +'</td>'+
-                        '<td>'+ element.status +'<button onclick="" type="button" id="confirmar"> confirmar</button></td>'+
+                        '<td id="trocar">'+element.status+'</td>'+
+                        '<td><button  onclick="confirmarEntrega('+index+')" id="confirmar" type="button">confirmar</button></td>'+
                         //'<td><button onclick="" type="button"> confirmar </td>'+
                         '<td><button onclick="verificar('+index+')" type="button">click</button></td>'
                         /* na função onclick eu inseri um botao que ao rodar a função verificar, ele me dá o valor do index(indice)
                         como ele vai acabar sendo igual ao valor de num pq o meu parametro passa a ser o mesmo index, ele passa a 
                         retornar o valor de false na validação do "if" o que o apaga da minha lista de cadastro (apenas os que eu clicar)
-                         
                         */
-                      '</tr>'/*usando o innerHTML para replicar os codigos HTML corfome for Replicado com forEach respeitando
+            
+                      '</tr>'
+                      
+                  
+
+
+                      
+                      
+                      
+                      /*usando o innerHTML para replicar os codigos HTML corfome for Replicado com forEach respeitando
                       a semantica entre string, variavel e formula*/
-                        
+                      
                         
                       })
+                     
 
     
   }
+   
   
   
--- pedidos.js
+++ pedidos.js
@@ -2,19 +2,20 @@
 // var pizzasObjetos = JSON.parse(pizzasDoStorage)
 // var precoDoStorage = localStorage.getItem("precoPizzas")
 // var precoObjetos = JSON.parse(precoDoStorage)
-
-var nome = document.getElementById("nome");
+function getElementNome (){
+  return document.getElementById("nome")
+}
+// var nome = document.getElementById("nome");
 var endereco = document.getElementById("endereco");
 var telefone = document.getElementById("telefone");
 var sabor = document.getElementById("sabor");
 var valor = document.getElementById("valor");
-var status1 = document.getElementById("status");
+// var status1 = document.getElementById("status");
 var btnEnviar = document.getElementById("btnEnviar");
 var infoDoStorage = localStorage.getItem("info");
 var infoObjeto = JSON.parse(infoDoStorage);
 var formularioSaboresStorage = localStorage.getItem("formularioSabores");
 var formularioSaboresObjeto = JSON.parse(formularioSaboresStorage);
-console.log(status1.value)
 console.log(formularioSaboresObjeto)
 loadselectPizza()
 
@@ -26,11 +27,11 @@ function loadselectPizza() {
     document.getElementById("sabor").innerHTML +=
       '<option  value=' + index + '>' + element.sabor2 + '</option>'
   })
-  document.getElementById("valor").value=formularioSaboresObjeto[0].valor2//fixei o valor do primeiro objeto
+  document.getElementById("valor").value = formularioSaboresObjeto[0].valor2//fixei o valor do primeiro objeto
 }
-function onChangeSaborPizza (){
+function onChangeSaborPizza() {
   let saborSelecionadoindex = document.getElementById("sabor").value
-  document.getElementById("valor").value=formularioSaboresObjeto[saborSelecionadoindex].valor2
+  document.getElementById("valor").value = formularioSaboresObjeto[saborSelecionadoindex].valor2
 
 
 }
@@ -44,8 +45,14 @@ btnEnviar.addEventListener("click", function (event) {
   var infoDoStorage = localStorage.getItem("info");
   var infoObjeto = JSON.parse(infoDoStorage);
 
-  let dados = { nome: nome.value, endereco: endereco.value, telefone: telefone.value,
-    sabor: formularioSaboresObjeto[sabor.value].sabor2, valor: formularioSaboresObjeto[sabor.value].valor2, status: status1.value };
+  let dados = {
+    nome: getElementNome().value,
+    endereco: endereco.value,
+    telefone: telefone.value,
+    sabor: formularioSaboresObjeto[sabor.value].sabor2,
+    valor: formularioSaboresObjeto[sabor.value].valor2,
+    status: "Pendente"
+  };
   console.log(infoDoStorage)
   if (infoDoStorage == null) {
     const lista = [dados]
@@ -54,11 +61,11 @@ btnEnviar.addEventListener("click", function (event) {
     infoObjeto.push(dados)
     localStorage.setItem("info", JSON.stringify(infoObjeto))
   }
-  nome.value = ""
+  getElementNome().value = ""
   endereco.value = ""
   telefone.value = ""
 
-  
+
 })
 
 
